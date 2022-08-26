b87b900a60004ca4c471145a73bc24acbc4807fa
YogashreeG <yogashree24@gmail.com>
2022-08-26 23:45:32 +0530

    Instagram Post Page

---
+++ src/compound/Website.js
@@ -0,0 +1,444 @@
+
+export function Website() {
+    return (
+      <>
+        <header>
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css"
+          />
+
+          <div className="logo">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png"
+              alt=""
+            />
+          </div>
+          <div className="search">
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <input type="search" placeholder="search"></input>
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#"></a>
+                <i class="fa-solid fa-house"></i>
+              </li>
+              <li>
+                <a href="#"></a>
+                <i class="fa-solid fa-message-text"></i>
+              </li>
+              <li>
+                <a href="#"></a>
+                <i class="fa-solid fa-square-plus"></i>
+              </li>
+              <li>
+                <a href="#"></a>
+                <i class="fa-solid fa-compass"></i>
+              </li>
+              <li>
+                <a href="#"></a>
+                <i class="fa-solid fa-heart"></i>
+              </li>
+              <li>
+                <a href="#"></a>
+                <img
+                  style={{ width: "22px" }}
+                  src="https://images.indulgexpress.com/uploads/user/imagelibrary/2017/12/11/original/1.jpg"
+                ></img>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <section>
+          <div className="left-side">
+            <div className="story">
+              <div className="stories">
+                <a href="#">
+                  <img
+                    style={{ width: "60px" }}
+                    src="https://celebs.infoseemedia.com/wp-content/uploads/2020/04/Virat-Kohli-with-wife-Anushka-Sharma-image.jpg"
+                    alt=""
+                  ></img>
+                </a>
+                <p>Friend</p>
+              </div>
+              <div className="stories">
+                <a href="#">
+                  <img
+                    style={{ width: "60px" }}
+                    src="https://celebs.infoseemedia.com/wp-content/uploads/2020/04/Virat-Kohli-with-wife-Anushka-Sharma-image.jpg"
+                    alt=""
+                  ></img>
+                </a>
+                <p>Jey</p>
+              </div>
+              <div className="stories">
+                <a href="#">
+                  <img
+                    style={{ width: "60px" }}
+                    src="https://celebs.infoseemedia.com/wp-content/uploads/2020/04/Virat-Kohli-with-wife-Anushka-Sharma-image.jpg"
+                    alt=""
+                  />
+                </a>
+                <p>krishna</p>
+              </div>
+              <div className="stories">
+                <a href="#">
+                  <img
+                    style={{ width: "60px" }}
+                    src="https://celebs.infoseemedia.com/wp-content/uploads/2020/04/Virat-Kohli-with-wife-Anushka-Sharma-image.jpg"
+                    alt=""
+                  />
+                </a>
+                <p>krishna</p>
+              </div>
+              <div className="stories">
+                <a href="#">
+                  <img
+                    style={{ width: "60px" }}
+                    src="https://celebs.infoseemedia.com/wp-content/uploads/2020/04/Virat-Kohli-with-wife-Anushka-Sharma-image.jpg"
+                    alt=""
+                  />
+                </a>
+                <p>krishna</p>
+              </div>
+              <div className="stories">
+                <a href="#">
+                  <img
+                    style={{ width: "60px" }}
+                    src="https://celebs.infoseemedia.com/wp-content/uploads/2020/04/Virat-Kohli-with-wife-Anushka-Sharma-image.jpg"
+                    alt=""
+                  />
+                </a>
+                <p>krishna</p>
+              </div>
+            </div>
+            <div className="posts">
+              <div className="post-title">
+                <div className="post-left">
+                  <div className="image">
+                    <img
+                      style={{ width: "32px", height: "32px" }}
+                      src="https://www.thestatesman.com/wp-content/uploads/2019/12/Virat-4.jpg"
+                    ></img>
+                  </div>
+                  <div className="details">
+                    <p className="name">virat</p>
+                    <p className="location">chennai</p>
+                  </div>
+                </div>
+                <div className="post-right">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+              <div className="post-content">
+                <img
+                  src="https://th.bing.com/th/id/OIP.oTwBpg0lcQH6x-bIIyOyjAHaFV?pid=ImgDet&rs=1"
+                  alt=""
+                  style={{ height: "525px", width: "525px" }}
+                ></img>
+              </div>
+              <div className="post-footer">
+                <div className="like-share-comment">
+                  <i class="fa-solid fa-heart"></i>
+                  <i class="fa-solid fa-comment"></i>
+                  <i class="fa-solid fa-paper-plane"></i>
+                </div>
+                <div className="save">
+                  <i class="fa-solid fa-bookmark"></i>
+                </div>
+              </div>
+              <div className="post-footer-content">
+                <p className="likes">100 likes</p>
+                <p className="names">
+                  wife<span>please</span>
+                </p>
+                <p className="comments">view all 30 comments</p>
+                <p className="posting-time">24 hours</p>
+              </div>
+              <div className="add-comment">
+                <div className="left-side">
+                  <i class="fa-solid fa-face-grin-hearts"></i>
+                  <input type="text" placeholder=" Add a commend"></input>
+                </div>
+                <div className="right-side">
+                  <p>Post</p>
+                </div>
+              </div>
+            </div>
+            <div className="posts">
+              <div className="post-title">
+                <div className="post-left">
+                  <div className="image">
+                    <img
+                      style={{ width: "32px", height: "32px" }}
+                      src="https://www.thestatesman.com/wp-content/uploads/2019/12/Virat-4.jpg"
+                    ></img>
+                  </div>
+                  <div className="details">
+                    <p className="name">virat</p>
+                    <p className="location">chennai</p>
+                  </div>
+                </div>
+                <div className="post-right">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+              <div className="post-content">
+                <img
+                  src="https://images.indulgexpress.com/uploads/user/imagelibrary/2017/12/11/original/1.jpg"
+                  alt=""
+                  style={{ height: "525px", width: "525px" }}
+                ></img>
+              </div>
+              <div className="post-footer">
+                <div className="like-share-comment">
+                  <i class="fa-solid fa-heart"></i>
+                  <i class="fa-solid fa-comment"></i>
+                  <i class="fa-solid fa-paper-plane"></i>
+                </div>
+                <div className="save">
+                  <i class="fa-solid fa-bookmark"></i>
+                </div>
+              </div>
+              <div className="post-footer-content">
+                <p className="likes">100 likes</p>
+                <p className="names">
+                  Cute<span></span>
+                </p>
+                <p className="comments">view all 30 comments</p>
+                <p className="posting-time">24 hours</p>
+              </div>
+              <div className="add-comment">
+                <div className="left-side">
+                  <i class="fa-solid fa-face-grin-hearts"></i>
+                  <input type="text" placeholder=" Add a commend"></input>
+                </div>
+                <div className="right-side">
+                  <p>Post</p>
+                </div>
+              </div>
+            </div>
+            <div className="posts">
+              <div className="post-title">
+                <div className="post-left">
+                  <div className="image">
+                    <img
+                      style={{ width: "32px", height: "32px" }}
+                      src="https://www.thestatesman.com/wp-content/uploads/2019/12/Virat-4.jpg"
+                    ></img>
+                  </div>
+                  <div className="details">
+                    <p className="name">virat</p>
+                    <p className="location">chennai</p>
+                  </div>
+                </div>
+                <div className="post-right">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+              <div className="post-content">
+                <img
+                  src="https://femina.wwmindia.com/content/2018/dec/01in1544513128.jpg"
+                  alt=""
+                  style={{ height: "525px", width: "525px" }}
+                ></img>
+              </div>
+              <div className="post-footer">
+                <div className="like-share-comment">
+                  <i class="fa-solid fa-heart"></i>
+                  <i class="fa-solid fa-comment"></i>
+                  <i class="fa-solid fa-paper-plane"></i>
+                </div>
+                <div className="save">
+                  <i class="fa-solid fa-bookmark"></i>
+                </div>
+              </div>
+              <div className="post-footer-content">
+                <p className="likes">100 likes</p>
+                <p className="names">
+                  My love<span></span>
+                </p>
+                <p className="comments">view all 30 comments</p>
+                <p className="posting-time">24 hours</p>
+              </div>
+              <div className="add-comment">
+                <div className="left-side">
+                  <i class="fa-solid fa-face-grin-hearts"></i>
+                  <input type="text" placeholder=" Add a commend"></input>
+                </div>
+                <div className="right-side">
+                  <p>Post</p>
+                </div>
+              </div>
+            </div>
+            <div className="posts">
+              <div className="post-title">
+                <div className="post-left">
+                  <div className="image">
+                    <img
+                      style={{ width: "32px", height: "32px" }}
+                      src="https://www.thestatesman.com/wp-content/uploads/2019/12/Virat-4.jpg"
+                    ></img>
+                  </div>
+                  <div className="details">
+                    <p className="name">virat</p>
+                    <p className="location">chennai</p>
+                  </div>
+                </div>
+                <div className="post-right">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </div>
+              </div>
+              <div className="post-content">
+                <img
+                  src="https://www.dynamitenews.com/images/2020/11/05/virat-kohli-turns-32-cricket-fraternity-extends-birthday-wishes/5fa375512b459.jpeg"
+                  alt=""
+                  style={{ height: "525px", width: "525px" }}
+                ></img>
+              </div>
+              <div className="post-footer">
+                <div className="like-share-comment">
+                  <i class="fa-solid fa-heart"></i>
+                  <i class="fa-solid fa-comment"></i>
+                  <i class="fa-solid fa-paper-plane"></i>
+                </div>
+                <div className="save">
+                  <i class="fa-solid fa-bookmark"></i>
+                </div>
+              </div>
+              <div className="post-footer-content">
+                <p className="likes">100 likes</p>
+                <p className="names">
+                  Happy Face<span></span>
+                </p>
+                <p className="comments">view all 30 comments</p>
+                <p className="posting-time">24 hours</p>
+              </div>
+              <div className="add-comment">
+                <div className="left-side">
+                  <i class="fa-solid fa-face-grin-hearts"></i>
+                  <input type="text" placeholder=" Add a commend"></input>
+                </div>
+                <div className="right-side">
+                  <p>Post</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="right-side">
+            <div className="profile-title">
+              <div className="profile-left">
+                <div className="image">
+                  <img
+                    style={{ width: "56px", height: "56px" }}
+                    src="https://www.letuspublish.com/wp-content/uploads/2017/06/Shahrukh-Khan-Wallpaper.gif"
+                    alt=""
+                  />
+                </div>
+                <div className="details">
+                  <p className="name">sharukh khan</p>
+                </div>
+              </div>
+              <div className="profile-right">
+                <p>Switch</p>
+              </div>
+            </div>
+            <div className="suggestion">
+              <p>suggestion for you</p>
+              <p>See all</p>
+            </div>
+            <div className="suggestion-title">
+              <div className="suggestion-left">
+                <div className="image">
+                  <img
+                    style={{ width: "32px", height: "32px" }}
+                    src="https://bigstarbio.com/wp-content/uploads/2020/04/Suriya.jpg"
+                  ></img>
+                </div>
+                <div className="details">
+                  <p className="name">Suriya</p>
+                  <p className="location">chennai</p>
+                </div>
+              </div>
+              <div className="suggestion-right">
+                <p>Follow</p>
+              </div>
+            </div>
+            <div className="suggestion-title">
+              <div className="suggestion-left">
+                <div className="image">
+                  <img
+                    style={{ width: "32px", height: "32px" }}
+                    src="https://filmyfocus.com/wp-content/uploads/2020/08/Kamal-Hassan-looks-dapper-in-his-new-look1-1024x768.jpg"
+                  ></img>
+                </div>
+                <div className="details">
+                  <p className="name">kamal</p>
+                  <p className="location">chennai</p>
+                </div>
+              </div>
+              <div className="suggestion-right">
+                <p>Follow</p>
+              </div>
+            </div>
+            <div className="suggestion-title">
+              <div className="suggestion-left">
+                <div className="image">
+                  <img
+                    style={{ width: "32px", height: "32px" }}
+                    src="https://i0.wp.com/godofindia.com/wp-content/uploads/2017/05/rajinikanth-36.jpg"
+                  ></img>
+                </div>
+                <div className="details">
+                  <p className="name"> Rajini</p>
+                  <p className="location">chennai</p>
+                </div>
+              </div>
+              <div className="suggestion-right">
+                <p>Follow</p>
+              </div>
+            </div>
+            <div className="suggestion-title">
+              <div className="suggestion-left">
+                <div className="image">
+                  <img
+                    style={{ width: "32px", height: "32px" }}
+                    src="https://im.indiatimes.in/facebook/2019/Jul/ms_dhoni_himself_has_not_spoken_on_his_retirement_plans_1563350842.jpg"
+                  ></img>
+                </div>
+                <div className="details">
+                  <p className="name">MS Dhoni</p>
+                  <p className="location">Mumbai</p>
+                </div>
+              </div>
+              <div className="suggestion-right">
+                <p>Follow</p>
+              </div>
+            </div>
+            <div className="suggestion-title">
+              <div className="suggestion-left">
+                <div className="image">
+                  <img
+                    style={{ width: "32px", height: "32px" }}
+                    src="https://4.bp.blogspot.com/-kD_dsLxVkBY/WN-bBR7J9gI/AAAAAAAAE50/cVIexJ84uKYymjL2gmSAN4kEHj7ggDDPgCK4B/s1600/Dhanush%2Bprofile%2Bfamily%252C%2Bwiki%2BAge%252C%2BAffairs%252C%2BBiodata%252C%2BHeight%252C%2BWeight%252C%2Bwife%252C%2BBiography%2Bgo%2Bprofile5.jpg"
+                  ></img>
+                </div>
+                <div className="details">
+                  <p className="name">Dhanush</p>
+                  <p className="location">chennai</p>
+                </div>
+              </div>
+              <div className="suggestion-right">
+                <p>Follow</p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+        
+    
+}
